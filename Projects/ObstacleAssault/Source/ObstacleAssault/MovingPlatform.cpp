16c647aaa9268467a065b9c7fa5f5a4e1e3c47f8
UihwanLee <luh5063014@naver.com>
2022-11-03 02:53:28 +0900

    Section 3 - Obstacle Assault (New UE5 Content) Lecture
    UE5 C++ 소스 코드 업로드

---
+++ Projects/ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Move platform forwards
+		// Get Current location
+		// Add vector to that location
+		// Set the location
+	// Send platform back if gone too far
+		// Check how far we've moved
+		// Reverse direction of motion if gone too far
+
+	FVector CurrentLocation = GetActorLocation();
+
+	CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+
+	SetActorLocation(CurrentLocation);
+
+}
+
