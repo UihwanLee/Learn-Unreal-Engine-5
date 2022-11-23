e7f26ac1fbfd88a75e4389098454aa281e8b2c4f
UihwanLee <luh5063014@naver.com>
2022-11-23 22:03:34 +0900

    Section 5 - Toon Tanks (New UE5 Content) Lecture
    BasePawn c++파일 생성 및 BP_PawnTank && BP_PawnTurret 생성

---
+++ Projects/ToonTanks 5.0/Source/ToonTanks/BasePawn.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BasePawn.generated.h"
+
+UCLASS()
+class TOONTANKS_API ABasePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABasePawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	// 항상 헤더파일에 선언하는 것 대신 class 키워드 써서 필요할때만 불러오는 방법 (Forward Declaration)
+	// 다른 헤더파일에서 불러올 수 있기 때문에 무조건 헤더파일에 헤더파일을 불러오는 방법은 옳지 않다.
+	// 다만 디테일하게 수정하려는 접근이 있을 시 헤더파일이 필요하다. -> cpp에 작성하는 것이 효율적
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
